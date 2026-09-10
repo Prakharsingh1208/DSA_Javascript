5e2e62cb5e9ba61648ac8116657617a83547c597
Prakhar Singh <prakharsingh1208@gmail.com>
2026-09-10 16:28:47 +0530

    Time: 2 ms (70.04%), Space: 54.6 MB (60.82%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const seen = new Map();
+
+    for(let i=0;i<nums.length;i++){
+        
+        let compliment = target - nums[i];
+
+        if(seen.has(compliment)){
+            return [seen.get(compliment),i];
+        }
+
+        seen.set(nums[i],i);
+    }
+
+    return [];
+};
